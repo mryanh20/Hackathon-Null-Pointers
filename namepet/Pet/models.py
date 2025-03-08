7980ee0a1959cf6ce3e06b4cbb749ca1dc705e69
Miles H. <milesryanherndon@gmail.com>
2025-03-08 14:07:07 -0800

    AAAAAAAAAAAAAAAAAAAAAA

--- namepet/Pet/models.py
+++ namepet/Pet/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class adoptee(models.Model):
+    name = models.CharField(null = True, max_length=63)
+    breed = models.CharField(max_length=31)
+    age = models.IntegerField()
+    photo = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=255)
+    species = models.CharField(max_length=63)
