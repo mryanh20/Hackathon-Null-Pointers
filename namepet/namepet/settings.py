b939c9db73a85b3756cf2f111b6d41be52c5330d
Nate Weinstein <Nateswein@gmail.com>
2025-03-08 16:20:52 -0800

    yippee :)))))))

--- namepet/namepet/settings.py
+++ namepet/namepet/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'namepet.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'petdata',
+        'USER': 'postgres',
+        'PASSWORD': '[]pen3ideTe3thInsyde',
+        'HOST': 'localHost',
+        'PORT': '5432',
     }
 }
 
