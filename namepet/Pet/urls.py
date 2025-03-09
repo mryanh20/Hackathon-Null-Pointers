fb0b87167b77bd5b6000524cc024f0f0bad0054a
Miles H. <milesryanherndon@gmail.com>
2025-03-08 18:42:13 -0800

    Added new JSON return methods

--- namepet/Pet/urls.py
+++ namepet/Pet/urls.py
@@ -5,5 +5,8 @@ from . import views
 urlpatterns = [
     path("unnamed/", views.list_unnamed, name="list"),
     path("cats/", views.list_cats, name = "list"),
-    path("dogs/", views.list_dogs, name = "list")
+    path("dogs/", views.list_dogs, name = "list"),
+    path("jsondog/", views.json_dogs, name = "list"),
+    path("jsoncat/", views.json_cats, name = "list"),
+    path("jsonunnamed/", views.json_unnamed, name = "list"),
 ]
--- namepet/Pet/views.py
+++ namepet/Pet/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Adoptee
 from django.http import HttpResponse
+from django.http import JsonResponse
 
 # Create your views here.
 def list_unnamed(request):
@@ -12,7 +13,20 @@ def list_cats(request):
     return render(request, "list.html", {"adoptees": adoptees})
 
 def list_dogs(request):
-    print(request.GET.get('breed'))
     adoptees = Adoptee.objects.filter(species="Dog", name__isnull=True)
     return render(request, "list.html", {"adoptees": adoptees})
+
+def json_unnamed(request):
+    adoptees = list(Adoptee.objects.filter(name__isnull=True).values())
+    return JsonResponse(adoptees, safe=False)
+
+def json_cats(request):
+    adoptees = list(Adoptee.objects.filter(species="Cat", name__isnull=True).values())
+    return JsonResponse(adoptees, safe=False)
+
+def json_dogs(request):
+    adoptees = list(Adoptee.objects.filter(species="Dog", name__isnull=True).values())
+    return JsonResponse(adoptees, safe=False)
+
+
 
